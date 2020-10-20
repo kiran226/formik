b7ad3b5bdaa84256663816327796604e2647c28c
kiran <gandikotakiran99@gmail.com>
2020-10-20 12:35:02 +0530

    Form Schema Validation with yup

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 const initialValues={
     name:'',
     email:'',
     channel:'',
 };
-const validate= values =>{
-    let errors={};
-    if(!values.name){
-        errors.name="Required";
-    }
-    if(!values.email){
-        errors.email="Required";
-    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-        errors.email="Invalid email format"
-    }
-    if(!values.channel){
-        errors.channel="Required"
-    }
-return errors    
-};
+// const validate= values =>{
+//     let errors={};
+//     if(!values.name){
+//         errors.name="Required";
+//     }
+//     if(!values.email){
+//         errors.email="Required";
+//     }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+//         errors.email="Invalid email format"
+//     }
+//     if(!values.channel){
+//         errors.channel="Required"
+//     }
+// return errors    
+// };
+
+const validationSchema = Yup.object({
+    name : Yup.string().required("Required"),
+    email : Yup.string().email("Invalid email format").required("Required"),
+    channel : Yup.string().required("Required")
+})
 const onSubmit = values =>{
     console.log("Form Data", values);
 }
 export const YouTubeForm = () => {
     const formik= useFormik({
         initialValues,
-        validate,
+        // validate,
+        validationSchema,
         onSubmit
     });
     // console.log("form values", formik.values);
